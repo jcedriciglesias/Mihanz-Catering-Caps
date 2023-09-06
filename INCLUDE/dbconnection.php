ab2b51c5f20e6de5c126ed4dbb487fe1fa51a9bd
ivpenny <202010865@btech.ph.education>
2023-09-06 19:34:32 +0800

    push

--- INCLUDE/dbconnection.php
+++ INCLUDE/dbconnection.php
@@ -1,5 +1,17 @@
+
 <?php
+$servername = "localhost"; // Replace with your server name or IP address
+$username = "root"; // Replace with your database username
+$password = ""; // Replace with your database password
+$database = "mihanzcatering"; // Replace with your database name
 
-
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+    // Set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
 
 ?>
